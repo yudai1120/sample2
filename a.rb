08b8cad073b7644f2d91a981bc483733108aef16
Yudai <hurk.fmb@gmail.com>
2019-09-10 06:18:52 +0000

    test

---
+++ a.rb
@@ -0,0 +1,23 @@
+class People
+  def initialize
+   p "Peopleのインスタンスが作られました"
+  end
+  def self.human
+    p "私はPeopleクラスです"
+  end
+  def name=(value)
+    @name = value
+  end
+  def name
+    @name
+  end
+  attr_accessor :name
+end
+people = People.new
+People.human
+people.name ="私は目からビームが出せます"
+p people.name
+
+class Childpeople < People
+end
+Childpeople.name
