7d3c4231b795ea9fef9902522babc1d4d29f730f
Yudai <hurk.fmb@gmail.com>
2019-09-15 07:38:42 +0000

    test

--- pictgram/app/controllers/application_controller.rb
+++ pictgram/app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery width:  :exception
   
   add_flash_types :success, :info, :warning, :danger
+  
+ 
+  
+  def current_user
+     @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  helper_method :current_user, :logged_in?
+  
+  
 end
--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'session/new'
+  
   root "pages#index"
   get "pages/help"
   
   resources :users
+  get      "/login", to: "sessions#new"
+  post     "/login", to: "sessions#create"
+  delete  "/logout", to: "sessions#destroy"
 end
